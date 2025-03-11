d26b6288516f2389108c78b917275d086fd77dea
unknown <hmstech11@gmail.com>
2025-03-11 14:53:38 +0500

    hello wolrd

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,16 +17,16 @@ class UserController extends Controller
     }
     public function getAllUsers()
     {
-        // $users=User::all();
-        // $users=$users->map(function($user){
-        //     return [
-        //         'id'=>$user->id,
-        //         'name'=>$user->username,
-        //         'avatar'=>asset('storage/'.$user->profile_picture),
-        //     ];
-        // });
-        // return ResponseHelper::success($users, 'User data fetched successfully');
-        return response()->json(['message' => 'User data fetched successfully']);
+        $users=User::all();
+        $users=$users->map(function($user){
+            return [
+                'id'=>$user->id,
+                'name'=>$user->username,
+                'avatar'=>asset('storage/'.$user->profile_picture),
+            ];
+        });
+        return ResponseHelper::success($users, 'User data fetched successfully');
+        // return response()->json(['message' => 'User data fetched successfully']);
         // try {
         //     $data = $this->userService->getAllUsers();
         //     return ResponseHelper::success($data, 'User data fetched successfully');
