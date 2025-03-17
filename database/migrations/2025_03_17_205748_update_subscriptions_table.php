aad923f9f552636154af5f3fd8b71dc6c57cf8fc
unknown <hmstech11@gmail.com>
2025-03-18 01:58:40 +0500

    hello wolrd

---
+++ database/migrations/2025_03_17_205748_update_subscriptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->string('google_product_id')->nullable()->after('package_id');
+            $table->string('purchase_token')->unique()->after('google_product_id');
+            $table->timestamp('expires_at')->nullable()->after('renewal_date');
+        });
+
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->boolean('vip_status')->default(false)->change(); // Update VIP status to a boolean field
+        // });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('subscriptions', function (Blueprint $table) {
+            $table->dropColumn(['google_product_id', 'purchase_token', 'expires_at']);
+        });
+
+        // Schema::table('users', function (Blueprint $table) {
+        //     $table->string('vip_status')->default('not_active')->change(); // Revert VIP status
+        // });
+    }
+};
