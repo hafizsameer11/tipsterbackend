99f22dcfafe2942ca79df53ede1332f9c076363b
unknown <hmstech11@gmail.com>
2025-03-19 21:33:20 +0500

    hello wolrd

--- app/Repositories/RankingRepository.php
+++ app/Repositories/RankingRepository.php
@@ -101,7 +101,7 @@ class RankingRepository
                 'username' => $user ? $user->username : 'Unknown',
                 'profile_picture' => $user ? $user->profile_picture : null,
                 'win_rate' => '0%', // Return zero if no data is found
-                'win_amount' => null
+                'win_amount' => 0
             ];
         }
         $winnerAmounts = WinnersAmount::all()->keyBy('rank');
