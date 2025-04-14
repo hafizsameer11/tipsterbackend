4c69a96b936745e661f8a4768fc6bc4bd25c7694
unknown <hmstech11@gmail.com>
2025-04-15 00:40:55 +0500

    hello wolrd

--- app/Repositories/TipRepository.php
+++ app/Repositories/TipRepository.php
@@ -212,8 +212,8 @@ class TipRepository
 
     public function getAllVipTips()
     {
-        $startOfWeek = Carbon::now()->subWeeks(1)->startOfWeek()->format('Y-m-d');
-        $endOfWeek = Carbon::now()->subWeeks(1)->endOfWeek()->format('Y-m-d');
+        $startOfWeek = Carbon::now()->subWeeks(0)->startOfWeek()->format('Y-m-d');
+        $endOfWeek = Carbon::now()->subWeeks(0)->endOfWeek()->format('Y-m-d');
         Log::info("start of week $startOfWeek and $endOfWeek");
         $topUserIds = $this->getTop3UserIdsOfLastWeek();
         Log::info("Top and vip users", $topUserIds);
